63683b5afbf4b0c310ea7c5ff179a8fd588148b2
vedran <vedran.prpic1@gmail.com>
2025-04-09 21:49:29 +0200

    move env folder to src

--- src/app/shared/debug-overlay/debug-overlay.component.ts
+++ src/app/shared/debug-overlay/debug-overlay.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-debug-overlay',
