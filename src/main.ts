1aec79d8d8e9fbf29e624e32ce4bc995ea55f5bd
Tihomir Ascic <tihomirascic18@gmail.com>
2025-05-22 12:21:43 +0200

    Feature: add croatian language.

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 import { AppComponent } from './app.component';
 import 'handsontable/dist/handsontable.full.min.css';
+import { registerLocaleData } from '@angular/common';
+import localeHr from '@angular/common/locales/hr';
 
+
+registerLocaleData(localeHr);
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
