024422473909628ef1c36a7c0ac9fee63f9dfe60
Tihomir Ascic <tihomirascic18@gmail.com>
2025-06-10 08:40:16 +0200

    style(topbar): remove porton name and logo

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -32,7 +32,7 @@ import { SelectModule } from 'primeng/select';
             <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
                 <i class="pi pi-bars"></i>
             </button>
-            <a class="layout-topbar-logo" routerLink="/">
+            <!-- <a class="layout-topbar-logo" routerLink="/">
                 <svg viewBox="0 0 54 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path
                         fill-rule="evenodd"
@@ -51,7 +51,7 @@ import { SelectModule } from 'primeng/select';
                     </g>
                 </svg>
                 <span>PortonCare</span>
-            </a>
+            </a> -->
         </div>
 
         <div class="layout-topbar-actions">
