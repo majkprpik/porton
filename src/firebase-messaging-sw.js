41b53da1b74f7d43ffd4a18503d555bbda26ce53
Tihomir Ascic <tihomirascic18@gmail.com>
2025-06-18 09:38:45 +0200

    fix: firebase service worker not registered

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,6 +1,15 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.1/firebase-messaging-compat.js');
 
+firebase.initializeApp({
+  apiKey: "AIzaSyAO3m0m4of98-Jii4XNooFpjA2c0yyMoK0",
+  authDomain: "porton-23805.firebaseapp.com",
+  projectId: "porton-23805",
+  storageBucket: "porton-23805.firebasestorage.app",
+  messagingSenderId: "878411187876",
+  appId: "1:878411187876:web:01f1c867087aea815a0831",
+});
+
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
