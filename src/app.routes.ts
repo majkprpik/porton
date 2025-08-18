42cb951f28c37198cc21115f7982ae2d9409f569
Tihomir Ascic <tihomirascic18@gmail.com>
2025-08-18 12:23:49 +0200

    refactor: remove unnecessary files

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -199,7 +199,6 @@ export const appRoutes: Routes = [
                     ProfileRoles.Uprava
                 ] }
             },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
             { path: 'documentation', component: Documentation },
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
         ]
